6c33f736016684e6c8c9deac370ff7787c309896
suhassk-hash <sharmasuhas450@gmail.com>
2021-12-18 19:00:24 +0530

    About and Portfolio Edit

--- aa.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('contextmenu', function (e) { 
-    // do something here... 
-    e.preventDefault(); 
-  }, false);
-  $(document).ready(function() {
-    $('.image-link').magnificPopup({type:'image'});
-  });
-  
